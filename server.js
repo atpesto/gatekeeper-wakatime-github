6d8a891f9614cb3bb9a05bf661534f23250d2d0b
Nikhil Arora <narora200@gmail.com>
2018-07-15 16:50:05 +0530

    [Refactor]: Refactored server.js

--- server.js
+++ server.js
@@ -81,28 +81,6 @@ function authenticateGithub(code, cb) {
   req.on('error', function(e) { cb(e.message); });
 }
 
-/**
- * Handles logging to the console.
- * Logged values can be sanitized before they are logged
- *
- * @param {string} label - label for the log message
- * @param {Object||string} value - the actual log message, can be a string or a plain object
- * @param {boolean} sanitized - should the value be sanitized before logging?
- */
-function log(label, value, sanitized) {
-  value = value || '';
-  if (sanitized){
-    if (typeof(value) === 'string' && value.length > TRUNCATE_THRESHOLD){
-      console.log(label, value.substring(REVEALED_CHARS, 0) + REPLACEMENT);
-    } else {
-      console.log(label, REPLACEMENT);
-    }
-  } else {
-    console.log(label, value);
-  }
-}
-
-
 // Convenience for allowing CORS on routes - GET only
 app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -113,30 +91,26 @@ app.all('*', function (req, res, next) {
 
 
 app.get('/wakatime/authenticate/:code', function(req, res) {
-  log('authenticating code:', req.params.code, true);
+
   authenticateWakatime(req.params.code, function(err, token) {
     var result
     if ( err ) {
       result = {"error": err};
-      log(result.error);
     } else {
       result = {"token": token};
-      log("token", result.token, true);
     }
     res.json(result);
   });
 });
 
 app.get('/github/authenticate/:code', function(req, res) {
-  log('authenticating code:', req.params.code, true);
+
   authenticateGithub(req.params.code, function(err, token) {
     var result
     if ( err ) {
       result = {"error": err};
-      log(result.error);
     } else {
       result = {"token": token};
-      log("token", result.token, true);
     }
     res.json(result);
   });
